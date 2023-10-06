2dc28efd8f39f7fd95d11953c8b2232a303a657c
Shaun Dsouza <dsouzashaun717@gmail.com>
2023-10-06 17:56:18 +0530

    integrated blockchain with dummy frontend

---
+++ BlockchainFrontend/client/src/components/Error404/Error.jsx
@@ -0,0 +1,22 @@
+import { Container, Title, Text } from "@mantine/core";
+import classes from "./Error.module.css";
+
+export function Error404() {
+  return (
+    <Container className={classes.root}>
+      <div className={classes.inner}>
+        <div className={classes.content}>
+          <Title className={classes.title}>Nothing to see here</Title>
+          <Text
+            c="dimmed"
+            size="lg"
+            ta="center"
+            className={classes.description}
+          >
+            Connect your metamask account to access further.
+          </Text>
+        </div>
+      </div>
+    </Container>
+  );
+}
